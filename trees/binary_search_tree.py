a60c24895afdcd2fbe136e90aebbefaf8e18fa73
Ishan <contact@ishansharma.com>
2018-02-09 05:39:03 -0600

    Adding Binary Search Trees (partial)
    Supported operations:
    1. Creation
    2. Insertion
    3. Deletion
    4. Searching
    5. Depth first traversal

---
+++ trees/binary_search_tree.py
@@ -0,0 +1,212 @@
+# Python Data Structures and Algorithms
+# http://proquest.safaribooksonline.com.libproxy.utdallas.edu/book/programming/python/9781786467355/binary-trees/24fa378d_24f1_4e8f_bff2_cc1cdad5e10a_xhtml?uicode=utdallas
+
+
+class Node:
+    """
+    Node is just a container for data.
+    It hols references for other nodes. We are using binary tree node, so only left and right children
+    """
+    def __init__(self, data):
+        self.data = data
+        self.right_child = None
+        self.left_child = None
+
+
+class BST:
+    def __init__(self):
+        self.root_node = None
+
+    def find_min(self):
+        """
+        Since min is always te left most leaf node, keep traversing left children until we get a leaf
+
+        Takes O(h) time where h is height of the tree
+        :return: Node
+        """
+        current = self.root_node
+        while current.left_child:
+            current = current.left_child
+
+        return current
+
+    def find_max(self):
+        """
+        Max is the rightmost leaf node, we traver right children until we get to a leaf
+
+        Takes O(h) time
+        :return: Node
+        """
+        current = self.root_node
+        while current.right_child:
+            current = current.right_child
+
+        return current
+
+    def insert(self, data):
+        """
+        Insert the number into our tree
+        :param data: int value we want to insert
+        :return:
+        """
+        node = Node(data)
+
+        if self.root_node is None:
+            self.root_node = node
+        else:
+            current = self.root_node
+            parent = None
+            while True:
+                parent = current
+                if node.data < current.data:
+                    current = current.left_child
+                    if current is None:
+                        parent.left_child = node
+                        return
+                else:
+                    current = current.right_child
+                    if current is None:
+                        parent.right_child = node
+                        return
+
+    def get_node_with_parent(self, data):
+        """
+        Since we don't store the parent for a node, this function helps us find the parent.
+        Useful when removing nodes
+
+        :param data: int
+        :return: tuple
+        """
+        parent = None
+        current = self.root_node
+        if current is None:
+            return parent, None
+        while True:
+            if current.data == data:
+                return parent, current
+            elif current.data > data:
+                parent = current
+                current = current.left_child
+            else:
+                parent = current
+                current = current.right_child
+
+    def remove(self, data):
+        """
+        Remove the node with given data. O(h) complexity
+        :param data: int
+        :return:
+        """
+        parent, node = self.get_node_with_parent(data) # first, get the parent and current node
+
+        if parent is None and node is None:
+            return False
+
+        # count the children
+        children_count = 0
+
+        if node.left_child and node.right_child:
+            children_count = 2
+        elif (node.left_child is None) and (node.right_child is None):
+            children_count = 0
+        else:
+            children_count = 1
+
+        # handle different conditions
+        # if no children, we just remove the node.
+        # if 1 child, we just replace node with its child
+        # if 2 children, we have to do traversal and find smallest child in right subtree
+        if children_count == 0:
+            if parent:
+                # node has no children (leaf node), we detect its position relative to parent and set that to None
+                if parent.right_child is node:
+                    parent.right_child = None
+                else:
+                    parent.left_child = None
+            else:
+                self.root_node = None
+        elif children_count == 1:
+            # only one child, replace with child
+            next_node = None
+            if node.left_child:
+                next_node = node.left_child
+            else:
+                next_node = node.right_child
+
+            if parent:
+                if parent.left_child is node:
+                    parent.left_child = next_node
+                else:
+                    parent.right_child = next_node
+            else:
+                self.root_node = next_node
+        else:
+            parent_of_leftmost_node = None
+            leftmost_node = node.right_child
+            while leftmost_node.left_child:
+                parent_of_leftmost_node = leftmost_node
+                leftmost_node = leftmost_node.left_child
+
+            node.data = leftmost_node.data
+            if parent_of_leftmost_node.left_child == leftmost_node:
+                parent_of_leftmost_node.left_child = leftmost_node.right_child
+            else:
+                parent_of_leftmost_node.right_child = leftmost_node.right_child
+
+    def search(self, data):
+        """
+        Search for data in the tree. If found, return it. Else return None
+        :param data: int
+        :return: int
+        """
+        current = self.root_node
+        while True:
+            if current is None:
+                return None
+            elif current.data is data:
+                return data
+            elif current.data > data:
+                current = current.left_child
+            else:
+                current = current.right_child
+
+    def inorder(self, root_node):
+        """
+        Do in order traversal starting at a node
+        :param root_node: Node
+        :return: None
+        """
+        current = root_node
+        if current is None:
+            return
+
+        self.inorder(current.left_child)
+        print(current.data)
+        self.inorder(current.right_child)
+
+    def preorder(self, root_node):
+        """
+        Pre-order traversal
+        :param root_node: Node
+        :return: None
+        """
+        current = root_node
+        if current is None:
+            return
+        print(current.data)
+        self.preorder(current.left_child)
+        self.preorder(current.right_child)
+
+    def postorder(self, root_node):
+        """
+        Post-order traversal
+        :param root_node: Node
+        :return: None
+        """
+        current = root_node
+        if current is None:
+            return
+
+        self.postorder(current.left_child)
+        self.postorder(current.right_child)
+        print(current.data)
--- trees/tests.py
+++ trees/tests.py
@@ -1,18 +1,46 @@
-from trees import trees_basics as trees
+from trees import binary_search_tree as b
 
-# creating a few nodes
-n1 = trees.Node("root node")
-n2 = trees.Node("left child node")
-n3 = trees.Node("right child node")
-n4 = trees.Node("left grandchild node")
+# # creating a few nodes
+# n1 = trees.Node("root node")
+# n2 = trees.Node("left child node")
+# n3 = trees.Node("right child node")
+# n4 = trees.Node("left grandchild node")
+#
+# # connect the nodes to each other
+# n1.left_child = n2
+# n1.right_child = n3
+# n2.left_child = n4
+#
+# # traverse
+# current = n1
+# while current:
+#     print(current.data)
+#     current = current.left_child
 
-# connect the nodes to each other
-n1.left_child = n2
-n1.right_child = n3
-n2.left_child = n4
+tree = b.BST()
+tree.insert(7)
+tree.insert(1)
+tree.insert(3)
+tree.insert(6)
+tree.insert(5)
+tree.insert(2)
+tree.insert(8)
+tree.insert(10)
 
-# traverse
-current = n1
-while current:
-    print(current.data)
-    current = current.left_child
+tree.remove(6)
+
+print("searching for 0", tree.search(0))
+print("searching for 2", tree.search(2))
+print("searching for 5", tree.search(1))
+
+print("going to print the tree (inorder):")
+print(tree.inorder(tree.root_node))
+
+print("going to print the tree (pre-order):")
+print(tree.preorder(tree.root_node))
+
+print("going to print the tree(post-order):")
+print(tree.preorder(tree.root_node))
+
+res = tree.get_node_with_parent(1)
+
